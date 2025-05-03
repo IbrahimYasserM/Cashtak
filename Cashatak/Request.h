18b5552cf17968f1d65d574631acc1dfd7bfed28
Omar Ehab <amooraomar2005@gmail.com>
2025-05-04 01:40:41 +0300

    Implement initial structure of User.h

--- Cashatak/Request.h
+++ Cashatak/Request.h
@@ -1,5 +1,21 @@
 #pragma once
-class Request
-{
-};
+#include <string>
+#include "Event.h"
 
+class Request : public Event
+{
+private:
+    std::string message;
+
+public:
+    // Constructor
+    Request(int id, double amount, const std::string& sender,
+        const std::string& recipient, const std::string& status,
+        const std::string& message);
+
+    // Getter
+    std::string getMessage() const;
+
+    // Setter
+    void setMessage(const std::string& message);
+};
--- Cashatak/User.h
+++ Cashatak/User.h
@@ -1,5 +1,41 @@
 #pragma once
-class User
-{
-};
+#include "Account.h"
+#include "Transaction.h"
+#include "Business.h"
+#include "Request.h"
+#include <string>
+#include <vector>
 
+using namespace std;
+
+class User : public Account
+{
+private:
+    static long long accountIdCounter;   // Static counter for generating unique IDs
+    double balance;
+    vector<Transaction> completedTransactions;
+    vector<Request> pendingIncomingRequests;  // Pending requests that other users request it from me 
+    vector<Request> pendingOutgoingRequests;  // Pending requests that I request from other users
+
+public:
+    // Constructors
+    User();
+    User(const string& username, const string& email,
+        const string& password, const string& accountType,
+        double initialBalance = 0.0);
+    ~User();
+   
+    //getters and setters 
+    double getBalance() const;
+    void setBalance(double newBalance,string password);
+    void addBalance(double amount,string password);
+    void editPassword(string userName,string currentPass,string newPass);
+    
+    // Methods to handle transactions and requests
+    vector<Transaction>& getHistoryTransactions() const;
+    vector<Request>& getPendingIncomingRequests() const;
+    vector<Request>& getPendingOutgoingRequests() const;
+    void requestMoney(Request moneyRequest);
+    void sendMoney(Transaction sendMoney);
+
+};
